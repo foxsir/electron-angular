a20e36e3579de1f2f565eb876751149e24f023cf
foxsir <838394225@qq.com>
2021-08-08 13:55:15 +0800

    增加视频样式

--- src/app/factorys/message/message-video/message-video.component.ts
+++ src/app/factorys/message/message-video/message-video.component.ts
@@ -1,6 +1,13 @@
 import {Component, Input, OnInit} from '@angular/core';
 import ChatMsgEntity from "@app/models/ChatMsgEntity";
 
+interface VideoInfo {
+  fileName: string;
+  fileMd5: string;
+  src: string;
+  fileLength: number;
+}
+
 @Component({
   selector: 'app-message-video',
   templateUrl: './message-video.component.html',
@@ -8,10 +15,13 @@ import ChatMsgEntity from "@app/models/ChatMsgEntity";
 })
 export class MessageVideoComponent implements OnInit {
   @Input() chatMsg: ChatMsgEntity;
+  public videoInfo: VideoInfo;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.videoInfo = JSON.parse(this.chatMsg.text);
+    this.videoInfo.src = "http://120.79.90.66:8808/ShortVideoDownloader?user_uid=web400070&file_name=" + this.videoInfo.fileName;
   }
 
 }
