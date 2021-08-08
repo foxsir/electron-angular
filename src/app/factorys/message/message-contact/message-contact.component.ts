aac40bc46353d1fdf4b94c293b9dd5023892e0c5
foxsir <838394225@qq.com>
2021-08-08 13:15:24 +0800

    修改名片样式

--- src/app/factorys/message/message-contact/message-contact.component.ts
+++ src/app/factorys/message/message-contact/message-contact.component.ts
@@ -1,6 +1,11 @@
 import {Component, Input, OnInit} from '@angular/core';
 import ChatMsgEntity from "@app/models/ChatMsgEntity";
 
+interface UserContact {
+  uid: string;
+  nickName: string
+}
+
 @Component({
   selector: 'app-message-contact',
   templateUrl: './message-contact.component.html',
@@ -8,10 +13,12 @@ import ChatMsgEntity from "@app/models/ChatMsgEntity";
 })
 export class MessageContactComponent implements OnInit {
   @Input() chatMsg: ChatMsgEntity;
+  public userContact: UserContact;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.userContact = JSON.parse(this.chatMsg.text);
   }
 
 }
