4cc8e00a21f62404301dfb2000d6c9c049890cca
foxsir <838394225@qq.com>
2021-08-08 14:34:00 +0800

    增加头像

--- src/app/factorys/message/message-contact/message-contact.component.ts
+++ src/app/factorys/message/message-contact/message-contact.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import ChatMsgEntity from "@app/models/ChatMsgEntity";
+import {AvatarService} from "@services/avatar/avatar.service";
+import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
 
 interface UserContact {
   uid: string;
@@ -14,11 +16,19 @@ interface UserContact {
 export class MessageContactComponent implements OnInit {
   @Input() chatMsg: ChatMsgEntity;
   public userContact: UserContact;
+  public userAvatar: SafeResourceUrl;
 
-  constructor() { }
+  constructor(
+    private avatarService: AvatarService,
+    private dom: DomSanitizer,
+  ) { }
 
   ngOnInit(): void {
     this.userContact = JSON.parse(this.chatMsg.text);
+
+    this.avatarService.getAvatar(this.userContact.uid).then(res => {
+      this.userAvatar = this.dom.bypassSecurityTrustResourceUrl(res);
+    });
   }
 
 }
