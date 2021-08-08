2d230eb23c0997839bfdd14735cbd1305e8a38a4
foxsir <838394225@qq.com>
2021-08-08 14:34:12 +0800

    增加文件样式

--- src/app/factorys/message/message-file/message-file.component.ts
+++ src/app/factorys/message/message-file/message-file.component.ts
@@ -1,6 +1,13 @@
 import {Component, Input, OnInit} from '@angular/core';
 import ChatMsgEntity from "@app/models/ChatMsgEntity";
 
+interface FileInfo {
+  fileName: string;
+  fileMd5: string;
+  fileLength: number;
+  src: string;
+}
+
 @Component({
   selector: 'app-message-file',
   templateUrl: './message-file.component.html',
@@ -8,10 +15,12 @@ import ChatMsgEntity from "@app/models/ChatMsgEntity";
 })
 export class MessageFileComponent implements OnInit {
   @Input() chatMsg: ChatMsgEntity;
+  public fileInfo: FileInfo;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.fileInfo = JSON.parse(this.chatMsg.text);
   }
 
 }
