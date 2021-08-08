a8808c4e346fb281dc9e7398014e54c620797126
foxsir <838394225@qq.com>
2021-08-08 12:44:49 +0800

    增加emoji显示

--- src/app/factorys/message/message-text/message-text.component.ts
+++ src/app/factorys/message/message-text/message-text.component.ts
@@ -1,5 +1,7 @@
 import {Component, Inject, Input, OnInit} from '@angular/core';
 import ChatMsgEntity from "@app/models/ChatMsgEntity";
+import EmojiMap from "./EmojiMap";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-message-text',
@@ -9,10 +11,27 @@ import ChatMsgEntity from "@app/models/ChatMsgEntity";
 export class MessageTextComponent implements OnInit {
   @Input() chatMsg: ChatMsgEntity;
 
-  constructor(){
+  constructor(
+    private dom: DomSanitizer,
+  ){
   }
 
   ngOnInit() {
+    this.showEmoji();
+  }
+
+  showEmoji() {
+    const content = this.chatMsg.text;
+    content.split("/").forEach(item => {
+      const emoji = ["/", item].join("");
+      if(EmojiMap.hasOwnProperty(emoji)) {
+        const img = document.createElement("img");
+
+        img.className = 'emoji-icon';
+        img.src = ['/assets/emojis', EmojiMap[emoji]].join("/");
+        this.chatMsg.text = this.chatMsg.text.replace(emoji, img.outerHTML);
+      }
+    });
   }
 
 }
