65a7d3a17402cfc9588f441d5f7ca4645223154e
sunnyppkqr79 <sunnyppkqr79@gmail.com>
2021-08-13 12:29:49 +0800

    我的群组： 数据显示

--- src/app/modules/home/group/group.component.ts
+++ src/app/modules/home/group/group.component.ts
@@ -6,27 +6,26 @@ import HttpPresponseModel from "@app/interfaces/http-response.interface";
 import ChattingGroupModel from "@app/models/chatting-group.model";
 
 @Component({
-  selector: 'app-group',
-  templateUrl: './group.component.html',
-  styleUrls: ['./group.component.scss']
+    selector: 'app-group',
+    templateUrl: './group.component.html',
+    styleUrls: ['./group.component.scss']
 })
 export class GroupComponent implements OnInit {
-  private localUserInfo: LocalUserinfoModel = this.localUserService.localUserInfo;
-  public chattingGroup: ChattingGroupModel[];
+    private localUserInfo: LocalUserinfoModel = this.localUserService.localUserInfo;
+    public chattingGroup: ChattingGroupModel[];
 
-  constructor(
-    private restService: RestService,
-    private localUserService: LocalUserService,
-  ) {
-    this.restService.submitGetGroupsListFromServer(this.localUserInfo.userId).subscribe((res: HttpPresponseModel) => {
-      if (res.success) {
-        this.chattingGroup = JSON.parse(res.returnValue);
-      }
-    });
-  }
+    constructor(private restService: RestService, private localUserService: LocalUserService) {
+        this.restService.submitGetGroupsListFromServer(this.localUserInfo.userId).subscribe((res: HttpPresponseModel) => {
+            if (res.success) {
+                this.chattingGroup = JSON.parse(res.returnValue);
 
-  ngOnInit(): void {
+                console.log('chattingGroup: ', this.chattingGroup);
+            }
+        });
+    }
 
-  }
+    ngOnInit(): void {
+
+    }
 
 }
