bb7bfc675ed02698cdca290251f31b2925cb83ad
foxsir <838394225@qq.com>
2021-08-17 15:22:13 +0800

    获取头像增加默认参数：fileName 支持只通过id获取， 或者通过文件名称获取

--- src/app/services/avatar/avatar.service.ts
+++ src/app/services/avatar/avatar.service.ts
@@ -3,6 +3,8 @@ import {HttpService} from "@services/http/http.service";
 import {ImageTools} from "@app/common/image.tools";
 import {AppConfig} from "@app/config/config";
 import * as localforage from "localforage";
+import {FileService} from "@services/file/file.service";
+import DirectoryType from "@services/file/config/DirectoryType";
 
 @Injectable({
   providedIn: 'root'
@@ -13,21 +15,28 @@ export class AvatarService {
   public defaultLocalAvatar = this.appConfig.defaultLocalAvatar;
 
   constructor(
-    private http: HttpService
+    private http: HttpService,
+    private fileService: FileService
   ) {
   }
 
   /**
    * 获取用户头像，返回用户设置的或默认头像，并缓存到 indexedDB
    * @param user_uid
+   * @param fileName
    */
-  getAvatar(user_uid: string): Promise<any> {
-    const url = this.imageTools.avatar(user_uid);
+  getAvatar(user_uid: string, fileName: string = null): Promise<any> {
+    let url: string;
     return new Promise((resolve, reject) => {
       localforage.getItem("avatarMap").then(data => {
         if (data !== null && !!data[user_uid]) {
           resolve(data[user_uid]);
         } else {
+          if(fileName !== null) {
+            url = this.fileService.getFileUrl([DirectoryType.OSS_PORTRAIT, fileName].join("/"));
+          } else {
+            url = this.imageTools.avatar(user_uid);
+          }
           this.http.getContentLength(url).then(length => {
             if (Number(length) === 0) {
               resolve(this.defaultLocalAvatar);
