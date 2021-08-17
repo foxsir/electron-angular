e8d01866bfdddfa976a75d1b51ee302fdd9ae01f
foxsir <838394225@qq.com>
2021-08-17 11:01:47 +0800

    添加创建群组组件

---
+++ src/app/modules/home/create-group/create-group.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-create-group',
+  templateUrl: './create-group.component.html',
+  styleUrls: ['./create-group.component.scss']
+})
+export class CreateGroupComponent implements OnInit {
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  goBack() {
+    return this.router.navigate(['/home/message']);
+  }
+
+}
