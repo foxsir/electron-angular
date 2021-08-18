b45f5e1e70711204bb763ff305cef9cf56cdaff5
foxsir <838394225@qq.com>
2021-08-18 19:48:11 +0800

    更新好友搜索

--- src/app/modules/home/search-friend/search-friend.component.ts
+++ src/app/modules/home/search-friend/search-friend.component.ts
@@ -1,4 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {DomSanitizer} from "@angular/platform-browser";
+import {CacheService} from "@services/cache/cache.service";
+import {AvatarService} from "@services/avatar/avatar.service";
+import {RestService} from "@services/rest/rest.service";
+import {LocalUserService} from "@services/local-user/local-user.service";
+
+import backspaceIcon from "@app/assets/icons/backspace.svg";
+import backspaceActiveIcon from "@app/assets/icons/backspace-active.svg";
+import closeIcon from "@app/assets/icons/close.svg";
+import closeActiveIcon from "@app/assets/icons/close-active.svg";
+import FriendModel from "@app/models/friend.model";
+import NewHttpResponseInterface from "@app/interfaces/new-http-response.interface";
+import {SnackBarService} from "@services/snack-bar/snack-bar.service";
+
+interface SearchFriend {
+  friendId: number;
+  isFriend: 0;
+  lastLoginTime: string; // "2021-08-18 19:03:51"
+  nickname: string;
+  remark: string;
+  userAvatarFileName: string;
+  whatsUp: string;
+}
 
 @Component({
   selector: 'app-search-friend',
@@ -7,9 +31,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchFriendComponent implements OnInit {
 
-  constructor() { }
+  public closeIcon = this.dom.bypassSecurityTrustResourceUrl(closeIcon);
+  public closeActiveIcon = this.dom.bypassSecurityTrustResourceUrl(closeActiveIcon);
+  public backspaceIcon = this.dom.bypassSecurityTrustResourceUrl(backspaceIcon);
+  public backspaceActiveIcon = this.dom.bypassSecurityTrustResourceUrl(backspaceActiveIcon);
+
+  public searchFriend: string = "";
+  public searchFriendInfo: SearchFriend = null;
+
+  constructor(
+    private router: Router,
+    private dom: DomSanitizer,
+    private cacheService: CacheService,
+    private avatarService: AvatarService,
+    private restService: RestService,
+    private snackBarService: SnackBarService,
+    private localUserService: LocalUserService,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  goBack() {
+    return this.router.navigate(['/home/message']);
+  }
+
+  search() {
+    if (this.searchFriend.trim().length > 0) {
+      this.restService.getFriendSearch({friendAccount: this.searchFriend})
+        .subscribe((res: NewHttpResponseInterface<{userInfo: SearchFriend; result: number}>) => {
+          if(res.data.result > 0) {
+            this.searchFriendInfo = res.data.userInfo;
+          } else {
+            this.searchFriendInfo = null;
+            this.snackBarService.openMessage("没有找到匹配的用户");
+          }
+      });
+    }
+  }
+
+  friendRequest() {
+    this.snackBarService.openMessage("未完成");
   }
 
 }
