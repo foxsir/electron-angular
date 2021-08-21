b54ec5394ee9cd1a3b0a0eddcb16f53341d54386
foxsir <838394225@qq.com>
2021-08-21 11:06:03 +0800

    增加对话框 confirm alert

--- src/app/services/dialog/dialog.service.ts
+++ src/app/services/dialog/dialog.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
 import {ComponentType} from "@angular/cdk/portal";
 import {MatDialogConfig} from "@angular/material/dialog/dialog-config";
+import {ConfirmComponent} from "@app/shared/dialogs/confirm/confirm.component";
+import {AlertComponent} from "@app/shared/dialogs/alert/alert.component";
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +13,38 @@ export class DialogService {
     public dialog: MatDialog
   ) { }
 
+  openDialog(component: ComponentType<any>, config: MatDialogConfig = {}): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const dialogRef = this.dialog.open(component, config);
 
-  openDialog(component: ComponentType<any>, config: MatDialogConfig = {}) {
-    const dialogRef = this.dialog.open(component, config);
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
+      dialogRef.afterClosed().subscribe(result => {
+        resolve(result);
+      });
     });
   }
+
+  confirm(data: Partial<{title: string; text: string; confirm: string; cancel: string }>): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      this.openDialog(ConfirmComponent, {
+        width: '314px',
+        height: '164px',
+        data: data
+      }).then((res: boolean) => {
+        resolve(res);
+      });
+    });
+  }
+
+  alert(data: Partial<{title: string; text: string; confirm: string; cancel: string }>): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      this.openDialog(AlertComponent, {
+        width: '314px',
+        height: '164px',
+        data: data
+      }).then((res: boolean) => {
+        resolve(res);
+      });
+    });
+  }
+
 }
