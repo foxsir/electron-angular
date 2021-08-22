23d855ed28bded70d0a92cd8951116d9d6ceb7bb
foxsir <838394225@qq.com>
2021-08-22 22:32:54 +0800

    add demo

---
+++ src/app/modules/setting-dialogs/demo-dialog/demo-dialog.component.ts
@@ -0,0 +1,41 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import AlarmItemInterface from "@app/interfaces/alarm-item.interface";
+import ChatmsgEntityModel from "@app/models/chatmsg-entity.model";
+import {DialogService} from "@services/dialog/dialog.service"; // 必需
+
+@Component({
+  selector: 'app-demo-dialog',
+  templateUrl: './demo-dialog.component.html',
+  styleUrls: ['./demo-dialog.component.scss']
+})
+export class DemoDialogComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<DemoDialogComponent>, // 必需
+    @Inject(MAT_DIALOG_DATA) public data: object, // 必需
+    private dialogService: DialogService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  close() {
+    const result = {};
+    this.dialogRef.close(result);
+    // this.dialogRef.close(true);
+    // this.dialogRef.close(100);
+  }
+
+  // 示例
+  demo() {
+    const data = {
+
+    };
+    this.dialogService.openDialog(DemoDialogComponent, {data: data}).then((res: object) => {
+      console.dir(res);
+    });
+  }
+
+
+}
---
+++ src/app/modules/setting-dialogs/setting-dialogs.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DemoDialogComponent } from './demo-dialog/demo-dialog.component';
+
+
+
+@NgModule({
+  declarations: [
+    DemoDialogComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SettingDialogsModule { }
