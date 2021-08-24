3bfe8c8216a87d01824864048d89220ed659e9a5
sunnyppkqr79 <sunnyppkqr79@gmail.com>
2021-08-24 13:41:46 +0800

    二维码：显示昵称

--- src/app/modules/user-dialogs/qr-code/qr-code.component.ts
+++ src/app/modules/user-dialogs/qr-code/qr-code.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalUserService } from "@services/local-user/local-user.service";
+import { RestService } from "@services/rest/rest.service";
 
 @Component({
     selector: 'app-qr-code',
@@ -9,13 +10,21 @@ import { LocalUserService } from "@services/local-user/local-user.service";
 export class QrCodeComponent implements OnInit {
 
     public value = "none";
-    public qrcodesize = 250;
+    public qrcodesize = 260;
+    public whatsup = "";
 
-    constructor(private localUserService: LocalUserService) { }
+    constructor(private localUserService: LocalUserService, private restService: RestService) { }
 
     ngOnInit(): void {
         const localUserInfo = this.localUserService.getObj();
         this.value = localUserInfo.userId.toString();
+
+        console.log('二维码页面，用户信息：', localUserInfo);
+
+        this.restService.getUserBaseById(localUserInfo.userId.toString()).subscribe(res => {
+            console.log('QrCodeComponent result: ', res);
+            this.whatsup = res.data.whatSUp;
+        });
     }
 
 }
