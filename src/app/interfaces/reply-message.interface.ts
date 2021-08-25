3bf89172dab528c61f975205909b62c4044eaa34
foxsir <838394225@qq.com>
2021-08-25 18:07:51 +0800

    更新ReplyMessageType.msg 结构

--- src/app/interfaces/reply-message.interface.ts
+++ src/app/interfaces/reply-message.interface.ts
@@ -2,8 +2,18 @@ export interface ReplyMessageType {
   duration: number;
   fileLength: number;
   fileName: string; // 文件名称
-  msg: string; // 回复消息
+  msg: string; // 回复消息 {msgType: number; msgContent: string}
   msgType: number; // 原消息类型
   reply: string; // 原消息
   userName: string; // 消息发送人
+  content: string; // 合并消息标题
+  messages: ReplyMessageChildMessage[]; // 合并消息
 }
+
+export interface ReplyMessageChildMessage {
+  date: number;
+  sendId: string;
+  sendNicName: string;
+  text: string;
+  type: number;
+};
