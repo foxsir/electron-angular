a3601f11d107fa60a8298015abd3126c8f8c9626
foxsir <838394225@qq.com>
2021-08-25 22:10:22 +0800

    更新转发消息对话框

--- src/app/modules/user-dialogs/transmit-message/transmit-message.component.ts
+++ src/app/modules/user-dialogs/transmit-message/transmit-message.component.ts
@@ -3,6 +3,13 @@ import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import ChatmsgEntityModel from "@app/models/chatmsg-entity.model";
 import {CacheService} from "@services/cache/cache.service";
 import FriendModel from "@app/models/friend.model";
+import {MatSelectionList} from "@angular/material/list";
+import {ReplyMessageChildMessage} from "@app/interfaces/reply-message.interface";
+import {MessageService} from "@services/message/message.service";
+import {MsgType} from "@app/config/rbchat-config";
+import {MessageEntityService} from "@services/message-entity/message-entity.service";
+import AlarmItemInterface from "@app/interfaces/alarm-item.interface";
+import {CurrentChattingChangeService} from "@services/current-chatting-change/current-chatting-change.service";
 
 @Component({
   selector: 'app-transmit-message',
@@ -13,10 +20,15 @@ export class TransmitMessageComponent implements OnInit {
   public filterFriend: string;
   public friendList: FriendModel[] = [];
 
+  public selectedFriends: FriendModel[] = [];
+
   constructor(
     public dialogRef: MatDialogRef<TransmitMessageComponent>,
     @Inject(MAT_DIALOG_DATA) public data: ChatmsgEntityModel[],
-    private cacheService: CacheService
+    private cacheService: CacheService,
+    private messageService: MessageService,
+    private messageEntityService: MessageEntityService,
+    private currentChattingChangeService: CurrentChattingChangeService,
   ) { }
 
   ngOnInit(): void {
@@ -35,6 +47,58 @@ export class TransmitMessageComponent implements OnInit {
           return friend.nickname.includes(this.filterFriend);
         }) as FriendModel[];
       }
+    });
+  }
+
+  async nextStep(friendSelect: MatSelectionList) {
+    const names = [];
+    this.selectedFriends = [];
+    friendSelect.selectedOptions.selected.forEach(item => {
+      this.selectedFriends.push(item.value);
+      names.push(item.value.nickname);
+    });
+
+    // const merge: ReplyMessageChildMessage[] = [];
+    await this.data.forEach(msg => {
+      const newMsg: ReplyMessageChildMessage = {
+        date: msg.date,
+        sendId: msg.uid,
+        sendNicName: msg.name,
+        text: msg.text,
+        type: msg.msgType,
+      };
+      this.selectedFriends.forEach(friend => {
+        this.messageService.sendMessage(MsgType.TYPE_TEXT, friend.friendUserUid, newMsg.text).then((res) => {
+          if(res.success === true) {
+            const alarmData: AlarmItemInterface = {
+              alarmItem: {
+                alarmMessageType: 0,
+                dataId: friend.friendUserUid.toString(),
+                date: new Date().getTime().toString(),
+                istop: true,
+                msgContent: newMsg.text,
+                title: friend.nickname,
+                avatar: null,
+              },
+              metadata: {
+                chatType: 'friend'
+              }
+            };
+            const message = res.msgBody.m;
+            const chatMsgEntity = this.messageEntityService.prepareSendedMessage(
+              message, 0, res.fingerPrint, MsgType.TYPE_TEXT
+            );
+            chatMsgEntity.isOutgoing = false;
+            this.cacheService.getCacheFriends().then(list => {
+              const fm: FriendModel = list[alarmData.alarmItem.dataId];
+              alarmData.alarmItem.avatar = fm.userAvatarFileName;
+              this.cacheService.putChattingCache(alarmData, chatMsgEntity).then(() => {
+                this.currentChattingChangeService.switchCurrentChatting(alarmData);
+              });
+            });
+          }
+        });
+      });
     });
   }
 
