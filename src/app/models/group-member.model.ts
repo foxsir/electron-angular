bd5462a8ed7aad16c6ee4ba4277fbb18a9d79840
foxsir <838394225@qq.com>
2021-08-27 00:25:51 +0800

    群成员模型

---
+++ src/app/models/group-member.model.ts
@@ -0,0 +1,9 @@
+export class GroupMemberModel {
+  g_id: string;
+  groupUserId: string;
+  isAdmin: string;
+  mute: string;
+  nickname: string;
+  userAvatarFileName: string;
+  user_uid: string;
+}
