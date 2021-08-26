f01c563bc048f058ba47226f05773e8f8fc097ab
foxsir <838394225@qq.com>
2021-08-26 12:33:10 +0800

    增加复制图片和文字

--- src/app/services/element/element.service.ts
+++ src/app/services/element/element.service.ts
@@ -16,4 +16,42 @@ export class ElementService {
   selectMessage(directive: boolean) {
     this.selectMessageSource.next(directive);
   }
+
+  copyTextToClipboard(container: HTMLDivElement) {
+    try {
+      const blob = new Blob([container.innerText], { type: 'text/plain' });
+      return this.setToClipboard(blob);
+    } catch (error) {
+      console.error('Something wrong happened');
+    }
+  }
+
+  copyImageToClipboard(img: HTMLImageElement) {
+    // take any image
+    // make <canvas> of the same size
+    const canvas = document.createElement('canvas');
+    canvas.width = img.naturalWidth;
+    canvas.height = img.naturalHeight;
+
+    const context = canvas.getContext('2d');
+
+    // copy image to it (this method allows to cut image)
+    context.drawImage(img, 0, 0);
+
+    // toBlob is async operation, callback is called when done
+    canvas.toBlob((blob) => {
+      try {
+        this.setToClipboard(blob);
+      } catch (error) {
+        console.error('Something wrong happened');
+        console.error(error);
+      }
+    }, 'image/png'); // 只支持png
+  }
+
+  private setToClipboard(blob) {
+    const data = [new ClipboardItem({ [blob.type]: blob })];
+    return navigator.clipboard.write(data);
+  }
+
 }
