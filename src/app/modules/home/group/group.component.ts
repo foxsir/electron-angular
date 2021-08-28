621e1ac7c1c3b54c82a5be2f167022a2322a54d5
foxsir <838394225@qq.com>
2021-08-28 21:35:39 +0800

    群列表改为使用缓存

--- src/app/modules/home/group/group.component.ts
+++ src/app/modules/home/group/group.component.ts
@@ -1,27 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {RestService} from "@services/rest/rest.service";
 import LocalUserinfoModel from "@app/models/local-userinfo.model";
 import {LocalUserService} from "@services/local-user/local-user.service";
 import HttpPresponseModel from "@app/interfaces/http-response.interface";
 import ChattingGroupModel from "@app/models/chatting-group.model";
+import GroupInfoModel from "@app/models/group-info.model";
+import {CacheService} from "@services/cache/cache.service";
+import {Subscribable, Subscription} from "rxjs";
 
 @Component({
     selector: 'app-group',
     templateUrl: './group.component.html',
     styleUrls: ['./group.component.scss']
 })
-export class GroupComponent implements OnInit {
+export class GroupComponent implements OnInit, OnDestroy {
     private localUserInfo: LocalUserinfoModel = this.localUserService.localUserInfo;
-    public chattingGroup: any[];
+    public chattingGroup: GroupInfoModel[] = [];
+    private subscribe: Subscription;
 
-    constructor(private restService: RestService, private localUserService: LocalUserService) {
-        this.restService.getUserJoinGroup().subscribe(res => {
-            this.chattingGroup = res.data;
-        });
+    constructor(
+      private restService: RestService,
+      private localUserService: LocalUserService,
+      private cacheService: CacheService
+    ) {
+      this.cacheService.getCacheGroups().then(data => {
+        this.chattingGroup = Object.values(data);
+      });
+
+      this.subscribe = this.cacheService.cacheUpdate$.subscribe(cache => {
+        if(cache.groupList) {
+          this.chattingGroup = Object.values(cache.groupList);
+        }
+      });
     }
 
     ngOnInit(): void {
+    }
 
+    ngOnDestroy() {
+      this.subscribe.unsubscribe();
     }
 
 }
