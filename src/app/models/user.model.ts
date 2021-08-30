a90c0e989895c92811bc185e6bea42169b64f1d2
foxsir <838394225@qq.com>
2021-08-30 14:18:57 +0800

    修改字段类型

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -2,9 +2,9 @@ export class UserModel {
   userUid: number; //用户id
   userMail: string; //账号
   nickname: string; //昵称
-  userAvatarFileName: null; //头像
+  userAvatarFileName: string; //头像
   userType: number; //用户类型 0普通用户， 1普通管理员，2超级管理员  3客服
-  userPhone: null; //手机号
+  userPhone: string; //手机号
   balance: number; //余额
   //用户等级
   userLevel: {
@@ -49,13 +49,13 @@ export class UserModel {
   myCommunicationNumber: string; //邀请码
   userCornet: string; //通讯号
   googleSecret: string; //谷歌验证器
-  latestLoginAddres: null; //上次登录地址
-  registerAddres: null; //注册地址
-  latestLoginIp: null; //上次登录ip
+  latestLoginAddres: string; //上次登录地址
+  registerAddres: string; //注册地址
+  latestLoginIp: string; //上次登录ip
   registerIp: string; //注册ip
-  online: null; //在线状态
-  token: null; //token
-  whatSUp: null; //个性签名
+  online: string; //在线状态
+  token: string; //token
+  whatSUp: string; //个性签名
   maxFriend: number; //最大好友数
   userSex: number; //用户性别1男0女
   userCornetChanged: string; //通讯号是否被修改过
