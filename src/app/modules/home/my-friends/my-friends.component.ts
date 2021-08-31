4d6e186eae1a1924f2f3f324b92aee7b2e0213ca
sunnyppkqr79 <sunnyppkqr79@gmail.com>
2021-08-31 15:46:07 +0800

    我的好友：显示列表数据，首字母排序，汉字

--- src/app/modules/home/my-friends/my-friends.component.ts
+++ src/app/modules/home/my-friends/my-friends.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CacheService } from "@services/cache/cache.service";
 import { RestService } from "@services/rest/rest.service";
 import FriendModel from "@app/models/friend.model";
+const iconv = window.require('iconv-lite');
 
 class FriendData {
     public char: string;
@@ -29,6 +30,9 @@ export class MyFriendsComponent implements OnInit {
     ngOnInit(): void {
         // 好友使用缓存中的数据
         // 我的群组组件有点击切换到聊天的示例 group.component.ts
+        //const buf = iconv.encode("你", 'gb2312');
+        //console.dir(buf);
+        //console.log('gb2312: ', this.getLetter('你'));
 
         this.cacheService.getCacheFriends().then(data => {
             if (data) {
@@ -37,17 +41,11 @@ export class MyFriendsComponent implements OnInit {
                 console.log('我的好友 list：', this.friendList);
 
                 for (let item of this.friendList) {
-                    var first_char = item.nickname[0].toString().toUpperCase();                    
+                    var first_char = item.nickname[0].toString().toUpperCase();
 
-                    //if (this.numbers.indexOf(first_char) != -1) {
-
-                    //}
-                    //else if (this.letters.indexOf(first_char) != -1) {
-
-                    //}
-                    //else {
-
-                    //}
+                    if (this.numbers.indexOf(first_char) == -1 && this.letters.indexOf(first_char) == -1) {
+                        first_char = this.getLetter(first_char);
+                    }
 
                     var results = this.frienddata.filter(t => t.char == first_char);
                     if (results.length == 0) {
@@ -75,6 +73,27 @@ export class MyFriendsComponent implements OnInit {
                 console.log('我的好友 list 格式化之后：', this.frienddata);
             }
         });
+    }
+
+    getLetter(str) {
+        const buf = iconv.encode(str, 'gb2312');
+        var num = parseInt(buf[0]) *256 + parseInt(buf[1]);
+
+        // iCnChar match the constant
+        var letter = "ABCDEFGHJKLMNOPQRSTWXYZ";
+        var areacode = [
+            45217, 45253, 45761, 46318, 46826, 47010, 47297, 47614,
+            48119, 49062, 49324, 49896, 50371, 50614, 50622, 50906,
+            51387, 51446, 52218, 52698, 52980, 53689, 54481, 55290];
+
+        for (var i = 0; i < 23; i++) {
+            if (areacode[i] <= num && num < areacode[i + 1]) {
+                str = letter.substr(i, 1);
+                break;
+            }
+        }
+
+        return str;
     }
 
 }
