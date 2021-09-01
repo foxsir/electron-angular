d3ccf7e227b539d3d96d54cd73c91fb898a7b65a
foxsir <838394225@qq.com>
2021-09-01 20:32:41 +0800

    完善引用服务

--- src/app/services/quote-message/quote-message.service.ts
+++ src/app/services/quote-message/quote-message.service.ts
@@ -4,7 +4,11 @@ import ChatmsgEntityModel from "@app/models/chatmsg-entity.model";
 import {ReplyMessageType} from "@app/interfaces/reply-message.interface";
 import {ReplyContentType} from "@app/interfaces/reply-content.interface";
 
-// 引用消息服务：用来设置和订阅引用回复
+/**
+ * 允许被回复的消息类型：文本，图片，视频，语音
+ * 回复消息时允许回复的消息类型：本文，图片，视频，语音 【PC端没有视频，语音】
+ * 引用消息服务：用来设置和订阅引用回复
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -19,13 +23,13 @@ export class QuoteMessageService {
     if(message === null) {
       this.messageSource.next(message);
     } else {
-      const chat = this.checkMessageIsPureText(message.text);
+      const chat = this.checkMessageIsPureText(message.text as string);
       if (chat === true) {
         this.messageSource.next(message);
       } else {
         // 如果是回复消息，需要构建新消息体
         const newChat = chat as ReplyMessageType;
-        message.text = newChat.msg;
+        message.text = JSON.stringify(newChat);
         this.messageSource.next(message);
       }
     }
