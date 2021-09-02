1f453417e0873f4a7920831c2c92e4f54e34f32e
sunnyppkqr79 <sunnyppkqr79@gmail.com>
2021-09-02 23:03:06 +0800

    聊天搜索： 搜索文件

--- src/app/modules/home/search-chatting/search-chatting.component.ts
+++ src/app/modules/home/search-chatting/search-chatting.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import ChattingModel from "@app/models/chatting.model";
 import AlarmItemInterface from "@app/interfaces/alarm-item.interface";
 import {CacheService} from "@services/cache/cache.service";
+import {formatDate} from "@app/libs/mobileimsdk-client-common";
 
 @Component({
     selector: 'app-search-chatting',
@@ -11,10 +12,12 @@ import {CacheService} from "@services/cache/cache.service";
 export class SearchChattingComponent implements OnInit {
     @Input() chatting: AlarmItemInterface;
 
-    currentTab: "chat" | "media" | "file" = "chat";
-    private keywords = "";
+    public formatDate = formatDate;
+    public currentTab: "chat" | "media" | "file" = "chat";
+    public keywords = "";
     
     public list_chat: ChattingModel[] = [];
+    public list_file: ChattingModel[] = [];
 
     constructor(
         private cacheService: CacheService
@@ -40,15 +43,26 @@ export class SearchChattingComponent implements OnInit {
             if (!!data) {
                 //this.ChattingModel = Object.values(data);
                 this.list_chat.length = 0;
+                this.list_file.length = 0;
 
                 for (let key in data){
                     var item = data[key];
                     if (item.msgType == 0 && item.text.indexOf(this.keywords) != -1) {
                         this.list_chat.push(item);
                     }
+
+                    if (item.msgType == 5) {
+                        var obj = JSON.parse(item.text);
+                        if (obj.fileName.indexOf(this.keywords) != -1) {
+                            item.fileName = obj.fileName;
+                            item.fileLength = obj.fileLength;
+                            this.list_file.push(item);
+                        }
+                    }
                 }
 
                 console.log("文字结果：", this.list_chat);
+                console.log("文件结果：", this.list_file);
             }
         });
     }
