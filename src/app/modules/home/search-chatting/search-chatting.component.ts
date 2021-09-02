f6f24f5c375eabbeb216cff3070c8f4385b2ce12
sunnyppkqr79 <sunnyppkqr79@gmail.com>
2021-09-02 22:50:07 +0800

    聊天搜索

--- src/app/modules/home/search-chatting/search-chatting.component.ts
+++ src/app/modules/home/search-chatting/search-chatting.component.ts
@@ -12,21 +12,44 @@ export class SearchChattingComponent implements OnInit {
     @Input() chatting: AlarmItemInterface;
 
     currentTab: "chat" | "media" | "file" = "chat";
+    private keywords = "";
+    
+    public list_chat: ChattingModel[] = [];
 
     constructor(
-        private cacheService: CacheService,
+        private cacheService: CacheService
     ) {
     }
 
     ngOnInit(): void {
+        
+    }
+
+    txtSearchChange(event: KeyboardEvent) {
+        console.log("txtSearchChange: ", event);
+
+        if (event.key != 'Enter') {
+            return;
+        }
+
+        console.log('回车确认：', this.keywords);
+
         this.cacheService.getChattingCache(this.chatting).then(data => {
+            console.log('消息列表：', data);
+
             if (!!data) {
-                this.ChattingModel = Object.values(data);
+                //this.ChattingModel = Object.values(data);
+                this.list_chat.length = 0;
 
+                for (let key in data){
+                    var item = data[key];
+                    if (item.msgType == 0 && item.text.indexOf(this.keywords) != -1) {
+                        this.list_chat.push(item);
+                    }
+                }
 
-
+                console.log("文字结果：", this.list_chat);
             }
         });
     }
-
 }
