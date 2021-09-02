104a5feba935734e6e93e07a7093a183d4f05036
sunnyppkqr79 <sunnyppkqr79@gmail.com>
2021-09-02 21:03:00 +0800

    聊天搜索

--- src/app/modules/home/search-chatting/search-chatting.component.ts
+++ src/app/modules/home/search-chatting/search-chatting.component.ts
@@ -1,21 +1,32 @@
 import {Component, Input, OnInit} from '@angular/core';
 import ChattingModel from "@app/models/chatting.model";
 import AlarmItemInterface from "@app/interfaces/alarm-item.interface";
+import {CacheService} from "@services/cache/cache.service";
 
 @Component({
-  selector: 'app-search-chatting',
-  templateUrl: './search-chatting.component.html',
-  styleUrls: ['./search-chatting.component.scss']
+    selector: 'app-search-chatting',
+    templateUrl: './search-chatting.component.html',
+    styleUrls: ['./search-chatting.component.scss']
 })
 export class SearchChattingComponent implements OnInit {
-  @Input() chatting: AlarmItemInterface;
+    @Input() chatting: AlarmItemInterface;
 
-  currentTab: "chat" | "media" | "file" = "chat";
+    currentTab: "chat" | "media" | "file" = "chat";
 
-  constructor() {
-  }
+    constructor(
+        private cacheService: CacheService,
+    ) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.cacheService.getChattingCache(this.chatting).then(data => {
+            if (!!data) {
+                this.ChattingModel = Object.values(data);
+
+
+
+            }
+        });
+    }
 
 }
