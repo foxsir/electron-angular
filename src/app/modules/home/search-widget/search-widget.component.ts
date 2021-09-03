3c56c03ccc6f6e677aeabe772ec82d6a1da08fb1
foxsir <838394225@qq.com>
2021-09-03 09:46:07 +0800

    更新全局搜索ui

--- src/app/modules/home/search-widget/search-widget.component.ts
+++ src/app/modules/home/search-widget/search-widget.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import editIcon from "@app/assets/icons/edit.svg";
+import closeIcon from "@app/assets/icons/close.svg";
+import closeActiveIcon from "@app/assets/icons/close-active.svg";
 import {DomSanitizer} from "@angular/platform-browser";
 import {Router} from "@angular/router";
 
@@ -10,8 +12,11 @@ import {Router} from "@angular/router";
   styleUrls: ['./search-widget.component.scss']
 })
 export class SearchWidgetComponent implements OnInit {
-
   public editIcon = this.dom.bypassSecurityTrustResourceUrl(editIcon);
+  public closeIcon = this.dom.bypassSecurityTrustResourceUrl(closeIcon);
+  public closeActiveIcon = this.dom.bypassSecurityTrustResourceUrl(closeActiveIcon);
+  public showResource: boolean = false;
+  public search: string;
 
   constructor(
     private dom: DomSanitizer,
@@ -27,6 +32,17 @@ export class SearchWidgetComponent implements OnInit {
 
   searchFriend() {
     return this.router.navigate(['/home/message/search-friend']);
+  }
+
+  searchContent() {
+    if(!this.showResource) {
+      this.showResource = !this.showResource;
+    }
+  }
+
+  cancelSearch() {
+    this.search = null;
+    this.showResource = false;
   }
 
 }
