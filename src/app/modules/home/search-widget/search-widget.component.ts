276e4e24232690f857f28e9ae98f8ac7344858b1
sunnyppkqr79 <sunnyppkqr79@gmail.com>
2021-09-04 18:59:10 +0800

    全局搜索

--- src/app/modules/home/search-widget/search-widget.component.ts
+++ src/app/modules/home/search-widget/search-widget.component.ts
@@ -7,42 +7,42 @@ import {DomSanitizer} from "@angular/platform-browser";
 import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-search-widget',
-  templateUrl: './search-widget.component.html',
-  styleUrls: ['./search-widget.component.scss']
+    selector: 'app-search-widget',
+    templateUrl: './search-widget.component.html',
+    styleUrls: ['./search-widget.component.scss']
 })
 export class SearchWidgetComponent implements OnInit {
-  public editIcon = this.dom.bypassSecurityTrustResourceUrl(editIcon);
-  public closeIcon = this.dom.bypassSecurityTrustResourceUrl(closeIcon);
-  public closeActiveIcon = this.dom.bypassSecurityTrustResourceUrl(closeActiveIcon);
-  public showResource: boolean = false;
-  public search: string;
+    public editIcon = this.dom.bypassSecurityTrustResourceUrl(editIcon);
+    public closeIcon = this.dom.bypassSecurityTrustResourceUrl(closeIcon);
+    public closeActiveIcon = this.dom.bypassSecurityTrustResourceUrl(closeActiveIcon);
+    public showResource: boolean = false;
+    public search: string;
 
-  constructor(
-    private dom: DomSanitizer,
-    private router: Router,
-  ) { }
+    constructor(
+        private dom: DomSanitizer,
+        private router: Router,
+    ) { }
 
-  ngOnInit(): void {
-  }
-
-  createGroup() {
-    return this.router.navigate(['/home/message/create-group']);
-  }
-
-  searchFriend() {
-    return this.router.navigate(['/home/message/search-friend']);
-  }
-
-  searchContent() {
-    if(!this.showResource) {
-      this.showResource = !this.showResource;
+    ngOnInit(): void {
     }
-  }
 
-  cancelSearch() {
-    this.search = null;
-    this.showResource = false;
-  }
+    createGroup() {
+        return this.router.navigate(['/home/message/create-group']);
+    }
+
+    searchFriend() {
+        return this.router.navigate(['/home/message/search-friend']);
+    }
+
+    searchContent() {
+        if (!this.showResource) {
+            this.showResource = !this.showResource;
+        }
+    }
+
+    cancelSearch() {
+        this.search = null;
+        this.showResource = false;
+    }
 
 }
