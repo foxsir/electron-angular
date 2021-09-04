0ed8f3b30bebfb6380aedf1ef5944ac1d434e47b
sunnyppkqr79 <sunnyppkqr79@gmail.com>
2021-09-04 19:30:02 +0800

    全局搜索

--- src/app/modules/home/search-widget/search-widget.component.ts
+++ src/app/modules/home/search-widget/search-widget.component.ts
@@ -3,8 +3,10 @@ import { Component, OnInit } from '@angular/core';
 import editIcon from "@app/assets/icons/edit.svg";
 import closeIcon from "@app/assets/icons/close.svg";
 import closeActiveIcon from "@app/assets/icons/close-active.svg";
-import {DomSanitizer} from "@angular/platform-browser";
-import {Router} from "@angular/router";
+import { DomSanitizer } from "@angular/platform-browser";
+import { Router } from "@angular/router";
+import { CacheService } from "@services/cache/cache.service";
+import AlarmItemInterface from "@app/interfaces/alarm-item.interface";
 
 @Component({
     selector: 'app-search-widget',
@@ -18,12 +20,26 @@ export class SearchWidgetComponent implements OnInit {
     public showResource: boolean = false;
     public search: string;
 
+    public alarmItemList: AlarmItemInterface[] = [];
+
     constructor(
         private dom: DomSanitizer,
         private router: Router,
+        private cacheService: CacheService
     ) { }
 
     ngOnInit(): void {
+        this.cacheService.getChattingList().then(res => {
+            if (res) {
+                var chatlist = Object.values(res);
+                console.log('聊天列表 01：', chatlist);
+            }
+
+            this.cacheService.SyncChattingList(res || {}).then(list => {
+                console.log('聊天列表 02：', list);
+                this.alarmItemList = list;
+            });
+        });
     }
 
     createGroup() {
@@ -43,6 +59,25 @@ export class SearchWidgetComponent implements OnInit {
     cancelSearch() {
         this.search = null;
         this.showResource = false;
+    }
+
+    txtSearchChange(event: KeyboardEvent) {
+        if (event.key != 'Enter') {
+            return;
+        }
+
+        console.log('回车确认：', this.search);
+
+        for (let alarmitem of this.alarmItemList) {
+            console.log('消息Model：', alarmitem);
+            this.cacheService.getChattingCache(alarmitem).then(data => {
+                console.log('消息列表：', data);
+
+                if (!!data) {
+
+                }
+            });
+        }
     }
 
 }
