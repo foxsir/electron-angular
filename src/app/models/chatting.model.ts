0b8f387cbf5965a78ee6b4ead53e3a8d94d2d72a
foxsir <838394225@qq.com>
2021-09-08 10:11:16 +0800

    istop to isTop

--- src/app/models/chatting.model.ts
+++ src/app/models/chatting.model.ts
@@ -5,7 +5,7 @@ export default class ChattingModel {
   alarmMessageType: number; // 0单聊 1临时聊天/陌生人聊天 2群聊
   dataId: string;
   date: string;
-  istop: boolean;
+  isTop: boolean;
   msgContent: string;
   title: string;
   avatar: string;
