d73379b0ae737b7baccfb6a35535b398f84c4688
foxsir <838394225@qq.com>
2021-09-09 21:52:53 +0800

    登录后完善 this.localUserService.localUserInfo 数据

--- src/app/modules/session/login/login.component.ts
+++ src/app/modules/session/login/login.component.ts
@@ -7,6 +7,9 @@ import {Router} from "@angular/router";
 import {LocalUserService} from "@services/local-user/local-user.service";
 import NewHttpResponseInterface from "@app/interfaces/new-http-response.interface";
 import {WindowService} from "@services/window/window.service";
+import {CacheService} from "@services/cache/cache.service";
+import {UserModel} from "@app/models/user.model";
+import LocalUserinfoModel from "@app/models/local-userinfo.model";
 
 @Component({
   selector: 'app-login',
@@ -21,6 +24,7 @@ export class LoginComponent implements OnInit {
     private snackBarService: SnackBarService,
     private localUserService: LocalUserService,
     private windowService: WindowService,
+    private cacheService: CacheService,
   ) {
   }
 
@@ -37,12 +41,44 @@ export class LoginComponent implements OnInit {
           this.router.navigate(["/home"]).then(() => {
             this.snackBarService.openMessage("登录成功");
             this.windowService.normalWindow();
+
+            this.cacheService.cacheMyInfo().then(() => {
+              // 使用缓存中的头像
+              this.cacheService.getMyInfo().then((data: UserModel) => {
+                this.updateLocalUserInfo(data);
+                // if(data.userAvatarFileName.length > 0) {
+                //   this.myAvatar = this.dom.bypassSecurityTrustResourceUrl(data.userAvatarFileName);
+                // }
+              });
+            });
+
           });
         } else {
           this.snackBarService.openMessage("你输入的账号或密码不正确，请重新输入");
         }
       });
     }
+  }
+
+  updateLocalUserInfo(data: UserModel) {
+    const local: Partial<LocalUserinfoModel> = {
+      latest_login_ip: data.latestLoginIp,
+      latest_login_time: null,
+      login: true,
+      maxFriend: data.maxFriend,
+      nickname: data.nickname,
+      online: data.online,
+      userAvatarFileName: data.userAvatarFileName,
+      userDesc: data.whatSUp,
+      userType: data.userType,
+      user_mail: data.userMail,
+      user_phone: data.userPhone,
+      user_sex: data.userSex,
+      whatsUp: data.whatSUp,
+    };
+    this.localUserService.update( Object.assign(local, this.localUserService.localUserInfo) );
+
+    console.dir(this.localUserService.localUserInfo);
   }
 
 }
