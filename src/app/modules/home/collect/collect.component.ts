0a8743e223060a86b900a8f6fbd57000cf03cef7
foxsir <838394225@qq.com>
2021-09-11 20:10:44 +0800

    完成右键 "发送给好友"

--- src/app/modules/home/collect/collect.component.ts
+++ src/app/modules/home/collect/collect.component.ts
@@ -8,6 +8,11 @@ import {DialogService} from "@services/dialog/dialog.service";
 import CollectModel from "@app/models/collect.model";
 import {MessageEntityService} from "@services/message-entity/message-entity.service";
 import ChatmsgEntityModel from "@app/models/chatmsg-entity.model";
+import {ForwardMessageService} from "@services/forward-message/forward-message.service";
+import {CacheService} from "@services/cache/cache.service";
+import {MessageService} from "@services/message/message.service";
+import {SelectFriendContactComponent} from "@modules/user-dialogs/select-friend-contact/select-friend-contact.component";
+import FriendModel from "@app/models/friend.model";
 
 interface CollectChatMsg {
   collect: CollectModel;
@@ -15,98 +20,97 @@ interface CollectChatMsg {
 }
 
 @Component({
-    selector: 'app-collect',
-    templateUrl: './collect.component.html',
-    styleUrls: ['./collect.component.scss']
+  selector: 'app-collect',
+  templateUrl: './collect.component.html',
+  styleUrls: ['./collect.component.scss']
 })
 export class CollectComponent implements OnInit {
-    collectList: CollectModel[] = [];
-    public show_modal = false;
-    public current_model: any;
+  collectList: CollectModel[] = [];
+  public show_modal = false;
+  public current_model: any;
 
-    public collectChatMsg: CollectChatMsg[] = [];
+  public collectChatMsg: CollectChatMsg[] = [];
 
-    public contextMenu = [
-        {
-            label: "发送给好友",
-            limits: "",
-            action: (item: any) => {
-                //this.copyTextToClipboard(messageContainer);
-                // console.log('发送给好友：', messageContainer);
-            }
-        },
-        {
-            label: "删除",
-            limits: "",
-            //action: (item: any, messageContainer: HTMLDivElement) => {
-            //    // this.copyImageToClipboard(messageContainer);
-            //    console.log('删除：', messageContainer);
-            //}
-            action: (item) => {
-                console.log('删除：', item);
-              // this.show_modal = true;
-                this.current_model = item;
-              this.dialogService.confirm({
-                title: '删除收藏',
-                text: '确认将此内容从收藏中删除？'
-              }).then((res: boolean) => {
-                if(res) {
-                  this.handleOk();
-                }
-              });
-            }
-        }
-    ];
-
-    constructor(
-        private restService: RestService,
-        private localUserService: LocalUserService,
-        private http: HttpService,
-        private contextMenuService: ContextMenuService,
-        private dialogService: DialogService,
-        private messageEntityService: MessageEntityService,
-    ) {
-        this.restService.getMyCollectList().subscribe(res => {
-            this.collectList = res.data;
-            this.collectList.forEach(item => {
-                const msgEntity = this.messageEntityService.prepareRecievedMessage(
-                    item.fromUserId, item.nickname, item.content, item.createTime, item.type
-                );
-                this.collectChatMsg.push({
-                  collect: item,
-                  chatMsg: msgEntity,
-                });
-            });
+  public contextMenu = [
+    {
+      label: "发送给好友",
+      action: (item: CollectChatMsg) => {
+        this.dialogService.openDialog(SelectFriendContactComponent).then((friend: FriendModel) => {
+          this.cacheService.generateAlarmItem(
+            friend.friendUserUid.toString(), 'friend', item.chatMsg.text, item.chatMsg.msgType
+          ).then(alarm => {
+            this.forwardMessageService.forward(alarm, item.chatMsg);
+          });
         });
-    }
-
-    ngOnInit(): void {
-
-    }
-
-    contextMenuForItem(e: MouseEvent, menu: MatMenuTrigger, span: HTMLSpanElement, chat: any) {
-        console.log('ddd');
-
-        menu.openMenu();
-        span.style.position = "fixed";
-        span.style.top = "0px";
-        span.style.left = "0px";
-        span.style.transform = `translate3d(${e.pageX}px, ${e.pageY}px, 0px)`;
-        return e.defaultPrevented;
-    }
-
-    handleCancel() {
-        this.show_modal = false;
-    }
-
-    handleOk() {
-        this.restService.deleteMissuCollectById(this.current_model.id).subscribe(res => {
-            this.show_modal = false;
-
-            this.restService.getMyCollectList().subscribe(res => {
-                this.collectList = res.data;
-            });
+      }
+    },
+    {
+      label: "删除",
+      action: (item: ChatmsgEntityModel) => {
+        console.log('删除：', item);
+        // this.show_modal = true;
+        this.current_model = item;
+        this.dialogService.confirm({
+          title: '删除收藏',
+          text: '确认将此内容从收藏中删除？'
+        }).then((res: boolean) => {
+          if (res) {
+            this.handleOk();
+          }
         });
+      }
     }
+  ];
+
+  constructor(
+    private restService: RestService,
+    private localUserService: LocalUserService,
+    private http: HttpService,
+    private contextMenuService: ContextMenuService,
+    private dialogService: DialogService,
+    private messageEntityService: MessageEntityService,
+    private forwardMessageService: ForwardMessageService,
+    private cacheService: CacheService,
+    private messageService: MessageService,
+  ) {
+    this.restService.getMyCollectList().subscribe(res => {
+      this.collectList = res.data;
+      this.collectList.forEach(item => {
+        const msgEntity = this.messageEntityService.prepareRecievedMessage(
+          item.fromUserId, item.nickname, item.content, item.createTime, item.type
+        );
+        this.collectChatMsg.push({
+          collect: item,
+          chatMsg: msgEntity,
+        });
+      });
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  contextMenuForItem(e: MouseEvent, menu: MatMenuTrigger, span: HTMLSpanElement, chat: any) {
+    menu.openMenu();
+    span.style.position = "fixed";
+    span.style.top = "0px";
+    span.style.left = "0px";
+    span.style.transform = `translate3d(${e.pageX}px, ${e.pageY}px, 0px)`;
+    return e.defaultPrevented;
+  }
+
+  handleCancel() {
+    this.show_modal = false;
+  }
+
+  handleOk() {
+    this.restService.deleteMissuCollectById(this.current_model.id).subscribe(res => {
+      this.show_modal = false;
+
+      this.restService.getMyCollectList().subscribe(res => {
+        this.collectList = res.data;
+      });
+    });
+  }
 
 }
