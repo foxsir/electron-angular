abe54fa35c47800b6249e17259470ed38d6bb3ef
foxsir <838394225@qq.com>
2021-09-13 21:52:18 +0800

    增加同意状态

--- src/app/models/friend-request.model.ts
+++ src/app/models/friend-request.model.ts
@@ -2,7 +2,8 @@ export class FriendRequestModel {
   reqDesc: string;
   reqTime: string;
   reqUserAvatar: string;
-  reqUserId: number
+  reqUserId: number;
   reqUserNickname:  string;
   userId: number;
+  agree: boolean = null; // 仅在本地有效
 }
