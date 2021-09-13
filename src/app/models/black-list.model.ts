305275826aa080ec44071665deb5e78815a674c9
foxsir <838394225@qq.com>
2021-09-13 21:51:50 +0800

    黑名单模型

---
+++ src/app/models/black-list.model.ts
@@ -0,0 +1,24 @@
+export default class BlackListModel {
+  balance: string;
+  googleSecret: string;
+  latestLoginAddres: string;
+  latestLoginIp: string;
+  maxFriend: string;
+  myCommunicationNumber: string;
+  nickname: string;
+  online: string;
+  reCommunicationNumber: string;
+  registerAddres: string;
+  registerIp: string;
+  token: string;
+  userAvatarFileName: string;
+  userCornet: string;
+  userCornetChanged: string;
+  userLevel: string;
+  userMail: string;
+  userPhone: string;
+  userSex: string;
+  userType: number;
+  userUid: number;
+  whatSUp: string;
+}
