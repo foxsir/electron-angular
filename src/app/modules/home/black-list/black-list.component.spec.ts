ff65d4d7da7ebcf7b6bfbbb5b321206122e847b7
foxsir <838394225@qq.com>
2021-09-13 21:52:59 +0800

    全局修改 mat-card-title 样式

--- src/app/modules/home/black-list/black-list.component.spec.ts
+++ src/app/modules/home/black-list/black-list.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { BlackListComponent } from './black-list.component';
+import {BlackListComponent} from './black-list.component';
 
 describe('BlackListComponent', () => {
   let component: BlackListComponent;
@@ -8,9 +8,9 @@ describe('BlackListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BlackListComponent ]
+      declarations: [BlackListComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
--- src/app/modules/home/black-list/black-list.component.ts
+++ src/app/modules/home/black-list/black-list.component.ts
@@ -1,49 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RestService} from "@services/rest/rest.service";
-import HttpPresponseModel from "@app/interfaces/http-response.interface";
+import {CacheService} from "@services/cache/cache.service";
+import BlackListModel from "@app/models/black-list.model";
+import {DialogService} from "@services/dialog/dialog.service";
+import NewHttpResponseInterface from "@app/interfaces/new-http-response.interface";
 
 @Component({
-    selector: 'app-black-list',
-    templateUrl: './black-list.component.html',
-    styleUrls: ['./black-list.component.scss']
+  selector: 'app-black-list',
+  templateUrl: './black-list.component.html',
+  styleUrls: ['./black-list.component.scss']
 })
 export class BlackListComponent implements OnInit {
 
-  blacklist: any[];
-  public show_modal = false;
-  public current_model: any;
+  blacklist: BlackListModel[] = [];
 
-  constructor(private restService: RestService) {
-      this.restService.getMyBlackList().subscribe(res => {
-          this.blacklist = res.data;
-      });
+  constructor(
+    private restService: RestService,
+    private cacheService: CacheService,
+    private dialogService: DialogService,
+  ) {
+    this.cacheService.getBlackList().then(cache => {
+      if(cache) {
+        cache.forEach(item => {
+          this.blacklist.push(item);
+        });
+      }
+    });
+    this.cacheService.cacheUpdate$.subscribe(data => {
+      if(data.blackListMap) {
+        this.blacklist = [];
+        data.blackListMap.forEach(item => {
+          this.blacklist.push(item);
+        });
+      }
+    });
   }
 
-    ngOnInit(): void {
-    }
+  ngOnInit(): void {
+  }
 
-    remove(item) {
-        this.show_modal = true;
-        this.current_model = item;
-    }
-
-    handleCancel() {
-        this.show_modal = false;
-    }
-
-    handleOk() {
-        var data = {
-            blackUserId: this.current_model.userUid,
+  remove(item) {
+    this.dialogService.confirm({text: ['将用户：', item.nickname, '移出黑名单'].join("")}).then(ok => {
+      if(ok) {
+        const data = {
+          blackUserId: item.userUid,
+          type: 0,
         };
-        data['type'] = 0;
-
-        this.restService.blackUser(data).subscribe(res => {
-            this.show_modal = false;
-
-            this.restService.getMyBlackList().subscribe(res => {
-                this.blacklist = res.data;
-            });
-        });
-    }
+        this.restService.blackUser(data).subscribe();
+      }
+    });
+  }
 
 }
