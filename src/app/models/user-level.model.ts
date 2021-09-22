2efa8d065d77220f95e9f4b739ccf0a01988ba3b
foxsir <838394225@qq.com>
2021-09-22 14:48:10 +0800

    从用户模型分离 UserLevelModel

---
+++ src/app/models/user-level.model.ts
@@ -0,0 +1,21 @@
+export default class UserLevelModel {
+  id: number; //
+  level: string; //
+  levelName: string; //
+  addGroup: string; //
+  sendRedpacket: string; //
+  addFriend: string; //
+  createGroup: string; //
+  addFriendSuper: string; //
+  addGroupSuper: string; //
+  addPersonSuper: string; //
+  groupDelMsg: string; //
+  seeYunMsg: string; //
+  msgTab: string; //
+  friendTab: string; //
+  circleTab: string; //
+  walletTab: string; //
+  collectionTab: string; //
+  findTab: string; //
+}
+
