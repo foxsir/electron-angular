65d70d760d51676f69457204e55d4b541ac8460f
foxsir <838394225@qq.com>
2021-09-24 13:18:01 +0800

    use Partial

--- src/app/interfaces/alarm-item.interface.ts
+++ src/app/interfaces/alarm-item.interface.ts
@@ -2,7 +2,7 @@ import ChattingModel from "../models/chatting.model";
 
 export default interface AlarmItemInterface {
   // 聊天信息
-  alarmItem: ChattingModel;
+  alarmItem: Partial<ChattingModel>;
   // 聊天元数据
   metadata: {
     chatType: string; // "friend" | "group"
