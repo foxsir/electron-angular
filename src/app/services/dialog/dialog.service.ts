78e1ec5b41463bc233ff145a53e4c045ab29f325
foxsir <838394225@qq.com>
2021-09-27 15:37:18 +0800

    去掉默认高度

--- src/app/services/dialog/dialog.service.ts
+++ src/app/services/dialog/dialog.service.ts
@@ -23,11 +23,11 @@ export class DialogService {
     });
   }
 
-    confirm(data: Partial<{ title: string; text: string; confirm: string; cancel: string, width: string, height: string }>): Promise<boolean> {
+    confirm(data: Partial<{ title: string; text: string; confirm: string; cancel: string; width: string; height: string }>): Promise<boolean> {
         return new Promise((resolve, reject) => {
             this.openDialog(ConfirmComponent, {
                 width: data.width === undefined ? '354px' : data.width + 'px',
-                height: data.height === undefined ? '150px' : data.height + 'px',
+                height: data.height ? data.height + 'px' : data.height,
                 data: data
             }).then((res: boolean) => {
                 resolve(res);
