8f5cf6898f12be1fd8a79db3ab0ac3f08714e187
foxsir <838394225@qq.com>
2021-09-30 18:05:12 +0800

    完善窗口加载

--- app/windowEventListen.ts
+++ app/windowEventListen.ts
@@ -21,7 +21,9 @@ export default class WindowEventListen {
         resolve(window)
       } else {
         setTimeout(() => {
-          return this.getFocusedWindow();
+          return this.getFocusedWindow().then(win => {
+            resolve(win);
+          });
         }, 100);
       }
     });
