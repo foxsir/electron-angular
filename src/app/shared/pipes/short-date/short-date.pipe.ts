9ff3b1fb0e918ffabb02cab336b2e79a1650522b
foxsir <838394225@qq.com>
2021-10-01 15:49:33 +0800

    mm:ss to hh:mm

--- src/app/shared/pipes/short-date/short-date.pipe.ts
+++ src/app/shared/pipes/short-date/short-date.pipe.ts
@@ -6,10 +6,10 @@ import {DatePipe} from "@angular/common";
 })
 export class ShortDatePipe implements PipeTransform {
 
-  transform(value: number, format: string = 'MM-d mm:ss'): string {
+  transform(value: number, format: string = 'MM-d hh:mm'): string {
     const date = new DatePipe("zh-Hans");
     if(this.isToday(value)) {
-      return date.transform(value, 'mm:ss');
+      return date.transform(value, 'hh:mm');
     } else {
       return date.transform(value, format);
     }
