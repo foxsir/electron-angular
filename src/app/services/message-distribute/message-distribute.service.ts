e2769d3cd0adac819362163fecec95fea89bbb54
foxsir <838394225@qq.com>
2021-10-04 19:21:06 +0800

    删除无用订阅

--- src/app/services/message-distribute/message-distribute.service.ts
+++ src/app/services/message-distribute/message-distribute.service.ts
@@ -8,41 +8,41 @@ import {UserProtocalsType} from "@app/config/rbchat-config";
 })
 export class MessageDistributeService {
 
-  private MT01_OF_ONLINE_NOTIVICATIONSource = new Subject();
+  private MT01_OF_ONLINE_NOTIVICATIONSource = new Subject<ProtocalModel>();
   public MT01_OF_ONLINE_NOTIVICATION$ = this.MT01_OF_ONLINE_NOTIVICATIONSource.asObservable();
 
-  private MT02_OF_OFFLINE_NOTIVICATIONSource = new Subject();
+  private MT02_OF_OFFLINE_NOTIVICATIONSource = new Subject<ProtocalModel>();
   public MT02_OF_OFFLINE_NOTIVICATION$ = this.MT02_OF_OFFLINE_NOTIVICATIONSource.asObservable();
 
-  private MT43_OF_TEMP$CHAT$MSG_SERVER$TO$BSource = new Subject();
+  private MT43_OF_TEMP$CHAT$MSG_SERVER$TO$BSource = new Subject<ProtocalModel>();
   public MT43_OF_TEMP$CHAT$MSG_SERVER$TO$B$ = this.MT43_OF_TEMP$CHAT$MSG_SERVER$TO$BSource.asObservable();
 
-  private MT45_OF_GROUP$CHAT$MSG_SERVER$TO$BSource = new Subject();
+  private MT45_OF_GROUP$CHAT$MSG_SERVER$TO$BSource = new Subject<ProtocalModel>();
   public MT45_OF_GROUP$CHAT$MSG_SERVER$TO$B$ = this.MT45_OF_GROUP$CHAT$MSG_SERVER$TO$BSource.asObservable();
 
-  private MT44_OF_GROUP$CHAT$MSG_A$TO$SERVERSource = new Subject();
+  private MT44_OF_GROUP$CHAT$MSG_A$TO$SERVERSource = new Subject<ProtocalModel>();
   public MT44_OF_GROUP$CHAT$MSG_A$TO$SERVER$ = this.MT44_OF_GROUP$CHAT$MSG_A$TO$SERVERSource.asObservable();
 
-  private MT46_OF_GROUP$SYSCMD_MYSELF$BE$INVITE_FROM$SERVERSource = new Subject();
+  private MT46_OF_GROUP$SYSCMD_MYSELF$BE$INVITE_FROM$SERVERSource = new Subject<ProtocalModel>();
   public MT46_OF_GROUP$SYSCMD_MYSELF$BE$INVITE_FROM$SERVER$ = this.MT46_OF_GROUP$SYSCMD_MYSELF$BE$INVITE_FROM$SERVERSource.asObservable();
 
-  private MT47_OF_GROUP$SYSCMD_COMMON$INFO_FROM$SERVERSource = new Subject();
+  private MT47_OF_GROUP$SYSCMD_COMMON$INFO_FROM$SERVERSource = new Subject<ProtocalModel>();
   public MT47_OF_GROUP$SYSCMD_COMMON$INFO_FROM$SERVER$ = this.MT47_OF_GROUP$SYSCMD_COMMON$INFO_FROM$SERVERSource.asObservable();
 
-  private MT48_OF_GROUP$SYSCMD_DISMISSED_FROM$SERVERSource = new Subject();
+  private MT48_OF_GROUP$SYSCMD_DISMISSED_FROM$SERVERSource = new Subject<ProtocalModel>();
   public MT48_OF_GROUP$SYSCMD_DISMISSED_FROM$SERVER$ = this.MT48_OF_GROUP$SYSCMD_DISMISSED_FROM$SERVERSource.asObservable();
 
-  private MT49_OF_GROUP$SYSCMD_YOU$BE$KICKOUT_FROM$SERVERSource = new Subject();
+  private MT49_OF_GROUP$SYSCMD_YOU$BE$KICKOUT_FROM$SERVERSource = new Subject<ProtocalModel>();
   public MT49_OF_GROUP$SYSCMD_YOU$BE$KICKOUT_FROM$SERVER$ = this.MT49_OF_GROUP$SYSCMD_YOU$BE$KICKOUT_FROM$SERVERSource.asObservable();
 
-  private MT50_OF_GROUP$SYSCMD_SOMEONEB$REMOVED_FROM$SERVERSource = new Subject();
+  private MT50_OF_GROUP$SYSCMD_SOMEONEB$REMOVED_FROM$SERVERSource = new Subject<ProtocalModel>();
   public MT50_OF_GROUP$SYSCMD_SOMEONEB$REMOVED_FROM$SERVER$ = this.MT50_OF_GROUP$SYSCMD_SOMEONEB$REMOVED_FROM$SERVERSource.asObservable();
 
-  private MT51_OF_GROUP$SYSCMD_GROUP$NAME$CHANGED_FROM$SERVERSource = new Subject();
+  private MT51_OF_GROUP$SYSCMD_GROUP$NAME$CHANGED_FROM$SERVERSource = new Subject<ProtocalModel>();
   public MT51_OF_GROUP$SYSCMD_GROUP$NAME$CHANGED_FROM$SERVER$ =
     this.MT51_OF_GROUP$SYSCMD_GROUP$NAME$CHANGED_FROM$SERVERSource.asObservable();
 
-  private MT06_OF_ADD_FRIEND_REQUEST_RESPONSE$FOR$ERROR_SERVER$TO$ASource = new Subject();
+  private MT06_OF_ADD_FRIEND_REQUEST_RESPONSE$FOR$ERROR_SERVER$TO$ASource = new Subject<ProtocalModel>();
   public MT06_OF_ADD_FRIEND_REQUEST_RESPONSE$FOR$ERROR_SERVER$TO$A$ =
     this.MT06_OF_ADD_FRIEND_REQUEST_RESPONSE$FOR$ERROR_SERVER$TO$ASource.asObservable();
 
@@ -168,9 +168,6 @@ export class MessageDistributeService {
         break;
       case UserProtocalsType.GROUP_SILENCE:
         this.GROUP_SILENCESource.next(originData);
-        break;
-      case UserProtocalsType.GROUP_ALL_SILENCE:
-        this.GROUP_ALL_SILENCESource.next(originData);
         break;
       default:
         this.illegalMessageSource.next(originData);
