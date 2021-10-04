08ad832ba266e342726ced79a218a077d73a7c7b
foxsir <838394225@qq.com>
2021-10-04 19:34:26 +0800

    显示全体禁言消息

--- src/app/factorys/message/message-no-talk/message-no-talk.component.ts
+++ src/app/factorys/message/message-no-talk/message-no-talk.component.ts
@@ -1,6 +1,15 @@
 import {Component, Input, OnInit} from '@angular/core';
 import ChatmsgEntityModel from "@app/models/chatmsg-entity.model";
 
+class NoTalk {
+  isBanned: boolean;
+  banTime: number;
+  sendId: string;
+  msg: string;
+  adminId: string;
+  uuid: string;
+}
+
 @Component({
   selector: 'app-message-no-talk',
   templateUrl: './message-no-talk.component.html',
@@ -9,9 +18,12 @@ import ChatmsgEntityModel from "@app/models/chatmsg-entity.model";
 export class MessageNoTalkComponent implements OnInit {
   @Input() chatMsg: ChatmsgEntityModel;
 
+  public noTalk: NoTalk;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.noTalk = JSON.parse(this.chatMsg.text);
   }
 
 }
