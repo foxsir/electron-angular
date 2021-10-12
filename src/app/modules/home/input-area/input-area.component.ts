f0ae3a5503432fb8625499c64cc69bb5748d8b6f
foxsir <838394225@qq.com>
2021-10-12 14:27:11 +0800

    先保存到数据库在发送

--- src/app/modules/home/input-area/input-area.component.ts
+++ src/app/modules/home/input-area/input-area.component.ts
@@ -327,23 +327,20 @@ export class InputAreaComponent implements OnInit, AfterViewInit {
         //   msgBody: res.msgBody
         // });
         // this.pushCache();
-
-        this.cacheService.putChattingCache(this.currentChat, chatMsgEntity, emitToUI).then(() => {
-          if(emitToUI) {
-            this.sendMessage.emit({chat: chatMsgEntity, dataContent: res.msgBody});
-          } else if(replaceEntity) {
-            const newMap: Map<string, ChatmsgEntityModel> = new Map();
-            this.cacheService.chatMsgEntityMap.forEach((v, k) => {
-              if(k === replaceEntity.fingerPrintOfProtocal) {
-                newMap.set(chatMsgEntity.fingerPrintOfProtocal, chatMsgEntity);
-              } else {
-                newMap.set(k, v);
-              }
-            });
-            this.cacheService.chatMsgEntityMap = newMap;
-            this.sendMessage.emit({chat: chatMsgEntity, dataContent: res.msgBody});
-          }
-        });
+        if(emitToUI) {
+          this.sendMessage.emit({chat: chatMsgEntity, dataContent: res.msgBody});
+        } else if(replaceEntity) {
+          const newMap: Map<string, ChatmsgEntityModel> = new Map();
+          this.cacheService.chatMsgEntityMap.forEach((v, k) => {
+            if(k === replaceEntity.fingerPrintOfProtocal) {
+              newMap.set(chatMsgEntity.fingerPrintOfProtocal, chatMsgEntity);
+            } else {
+              newMap.set(k, v);
+            }
+          });
+          this.cacheService.chatMsgEntityMap = newMap;
+          this.sendMessage.emit({chat: chatMsgEntity, dataContent: res.msgBody});
+        }
       }
     });
   }
