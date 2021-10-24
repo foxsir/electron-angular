9c567b10736ffb3ffdcf4455111c16c89b48f4ef
453957665@qq.com <Guojin780406>
2021-10-25 00:29:59 +0800

    群解散会话列表闪现问题调整

--- src/app/services/cache/cache.service.ts
+++ src/app/services/cache/cache.service.ts
@@ -249,17 +249,22 @@ export class CacheService extends DatabaseService {
     return new Promise<boolean>((resolve) => {
       this.getChattingList().then(list => {
         if(list.get(dataId)) {
+          const lastFp = list.get(dataId).alarmData.alarmItem.lastFp;
+          /*
+          this.saveData<LastMessageModel>({
+            model: 'lastMessage',
+            data: {
+              dataId: dataId, fp: lastFp
+            },
+            update: {dataId: dataId}
+          });
+          *
+           */
+          //list.delete(dataId);
+          this.cacheSource.next({alarmDataMap: list});
+
           this.deleteData<ChattingModel>({model: "chatting", query: {dataId: dataId}}).then(() => {
-            const lastFp = list.get(dataId).alarmData.alarmItem.lastFp;
-            this.saveData<LastMessageModel>({
-              model: 'lastMessage',
-              data: {
-               dataId: dataId, fp: lastFp
-              },
-              update: {dataId: dataId}
-            });
-            list.delete(dataId);
-            this.cacheSource.next({alarmDataMap: list});
+
             resolve(true);
           });
         }
