f1f93df9d59b7583f488e6d71ee6a4d38c949bfc
foxsir <838394225@qq.com>
2021-10-26 21:54:22 +0800

    修复接受@逻辑

--- src/app/modules/home/chatting-area/chatting-area.component.ts
+++ src/app/modules/home/chatting-area/chatting-area.component.ts
@@ -896,7 +896,7 @@ export class ChattingAreaComponent implements OnInit, AfterViewInit, AfterConten
 
   saveAtMsg(ty: number, dataId: string, chatMsgEntity: ChatmsgEntityModel) {
     //如果发送者与接收者相同，对本人不做@标记
-    if(chatMsgEntity.memberId.toString() === chatMsgEntity.uid.toString()) return;
+    if(chatMsgEntity.memberId.toString() === this.localUserInfo.userId.toString()) return;
     if(ty === MsgType.TYPE_AITE) {
       this.cacheService.putAtMessage(dataId, chatMsgEntity);
     }
